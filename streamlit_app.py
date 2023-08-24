38e5aa52e2f65f79708597dec3ce32cf05f097d6
antiwirusas <126700291+antiwirusas@users.noreply.github.com>
2023-08-24 10:11:30 +0100

    Update streamlit_app.py
    Added The Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 a 8ueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
