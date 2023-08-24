2cb5b80f4d09df94ba0a5dfdcafece3cc3f5c4f4
antiwirusas <126700291+antiwirusas@users.noreply.github.com>
2023-08-24 10:15:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
-
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 a 8ueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
